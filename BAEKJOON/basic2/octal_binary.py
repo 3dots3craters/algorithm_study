62d9c40bc498df7ee251e7a7d2cab6a2a4576082
3dots3craters <3dots3craters@gmail.com>
2022-01-06 14:59:47 +0900

    Feat: BAEKJOON 기초2 8진수 2진수 문제 풀이

---
+++ BAEKJOON/basic2/octal_binary.py
@@ -0,0 +1,12 @@
+
+num = int(input(), 8)
+print(bin(num)[2:])
+
+
+# 참고자료
+# https://security-nanglam.tistory.com/508(진수 변환 방법)
+# https://codechacha.com/ko/python-substring/(문자열 슬라이싱 방법)
+
+# 출처: https://www.acmicpc.net/source/37147037
+# a = int(input(), 8)
+# print(format(a, 'b'))
