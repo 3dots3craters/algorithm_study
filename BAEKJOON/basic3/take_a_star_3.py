079feae1e9d41bf4f85a345f5111ac78f3439027
3dots3craters <3dots3craters@gmail.com>
2022-01-12 09:22:50 +0900

    Feat: BAEKJOON 기초3 별찍기3 문제 풀이

---
+++ BAEKJOON/basic3/take_a_star_3.py
@@ -0,0 +1,12 @@
+N = int(input())
+for i in range(N):
+    print('*'.rjust(N - i, '*'))
+
+
+# 참고자료: https://brownbears.tistory.com/483
+
+# 출처: https://www.acmicpc.net/source/37386894
+# num = int(input())
+#
+# for i in range(num,0,-1):
+#     print("*"*i)
