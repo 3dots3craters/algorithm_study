c6768f1c743ca7db25de03121b2b3d1559a8d66e
3dots3craters <3dots3craters@gmail.com>
2022-01-21 10:05:16 +0900

    Feat: BAEKJOON 기초3 캥거루 세마리 문제 풀이

--- BAEKJOON/basic3/SKOCIMIS.py
+++ BAEKJOON/basic3/SKOCIMIS.py
@@ -1,21 +1,2 @@
 A, B, C = map(int, input().split())
-
-counts = []
-
-
-def jump(posA, posB, posC, cnt):
-    print("posA: ", posA, "posB: ", posB, "posC: ", posC)
-    for i in range(posA + 1, posB, 1):
-        cnt += 1
-        jump(posA, i, posB, cnt)
-        cnt -= 1
-    for j in range(posB + 1, posC, 1):
-        cnt += 1
-        jump(posB, j, posC, cnt)
-        cnt -= 1
-    counts.append(cnt)
-    print("-----------------")
-
-
-jump(A, B, C, 0)
-print(max(counts))
+print((B - A - 1) if (B - A - 1) > (C - B - 1) else (C - B - 1))
